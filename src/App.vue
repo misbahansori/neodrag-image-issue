dbc039564c416d26b79863533ad160ed6dc4f7d6
Misbah Ansori <misbah.ansori24@gmail.com>
2025-04-09 06:11:28 +0700

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,17 @@
 <script setup lang="ts">
-import RadixSelect from "./components/RadixSelect.vue";
-import RekaSelect from "./components/RekaSelect.vue";
+import { vDraggable } from "@neodrag/vue";
 </script>
 <template>
   <div class="mx-auto max-w-4xl p-20">
-    <div class="leading-relaxed flex flex-col gap-4">
-      <h1 class="text-2xl font-semibold">
-        Reka UI vs Radix Vue Select Value issue
-      </h1>
-      <p class="text-gray-500">
-        When using the <code>Select</code> components from Reka UI, the
-        <code>SelectValue</code> will not render elements except for the text.
-      </p>
-      <RekaSelect />
-
-      <hr class="my-2 border-gray-200" />
-
-      <p class="text-gray-500">
-        But when using the <code>Select</code> component from Radix Vue, the
-        <code>SelectValue</code> will render everything including the image and
-        text.
-      </p>
-      <RadixSelect />
-    </div>
-    <div class="flex flex-col gap-4 pt-6">
-      <p class="text-gray-500">
-        Note: The select implementation is the same for both components.
-        <br />
-        The only difference is the imports
-      </p>
-    </div>
+    <p class="text-center text-gray-500">
+      Drag the image to move it around. But it's not working correctly.
+    </p>
+    <img
+      v-draggable
+      draggable="true"
+      src="./assets/vue.svg"
+      alt="Vue"
+      class="size-40"
+    />
   </div>
 </template>
